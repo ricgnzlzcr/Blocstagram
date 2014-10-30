8a8e658d2158ea46e9456cc48640e24af65e2b72
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2014-10-30 10:51:52 -0400

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Ricardo Gonzalez on 10/13/14.
+//  Copyright (c) 2014 Ricardo Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
